799edad4f1a9c6c62eb4dd8dff6f42fd50a4caa5
Dong Yuwei <newdongyuwei@gmail.com>
2017-01-06 11:52:32 +0800

    add css-loader for webpack and showcase the HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 var path = require("path");
 module.exports = {
-  entry: {
-    app: ["./tests/input/entry.js"]
-  },
-  output: {
-    path: path.resolve(__dirname, "tests/output/webpack"),
-    publicPath: "/assets/",
-    filename: "entry_bundled.js"
-  }
+    entry: {
+        app: ["./tests/input/entry-webpack.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "tests/output/webpack"),
+        publicPath: "/assets/",
+        filename: "entry_bundled.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    }
 };
